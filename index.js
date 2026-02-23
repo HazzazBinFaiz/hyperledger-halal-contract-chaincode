65764ce57e7aea725fb25f616d49f6760c76ce4d
MD. Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2026-02-23 17:04:55 +0600

    Add to git

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+'use strict';
+
+const HalalTrackerContract = require('./lib/contract.js');
+
+module.exports.HalalTrackerContract = HalalTrackerContract;
+module.exports.contracts = [HalalTrackerContract];
